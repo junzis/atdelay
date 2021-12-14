509a7e6d52699f18cfa3677c6aeb9a2aefb349af
Niels Prins <57627835+Niels-Prins@users.noreply.github.com>
2021-12-14 16:13:30 +0100

    Made a (almost) final version of tool_box and small changes to RandomForest

--- randomForest/randomForest.py
+++ randomForest/randomForest.py
@@ -8,11 +8,15 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 sys.path.append(".")
-from tools.tool_box import filtering_data_ordinal_enc
-from tools.tool_box import parameter_search, get_data, plot
+from tools.tool_box import get_data, plot
 
 
 def grid_search_forest():
+    """ Performs grid search on a RandomForest estimator. DO NOT USE.
+
+    Returns:
+        dict: A dictionary containing results after GridSearch.
+    """    
     parameters = {
         "n_estimators": [100, 300, 600],
         "max_features": [None, "log2", "auto"],
@@ -46,8 +50,7 @@ if __name__ == "__main__":
         min_samples_split=10,
         min_samples_leaf=2,
         bootstrap=True,
-        n_jobs=-1,
-        verbose=3,
+        n_jobs=-1
     )
     forest.fit(X_train, y_train)
     prediction = forest.predict(X_test)
@@ -55,8 +58,8 @@ if __name__ == "__main__":
     predictions["real"] = y_test
     predictions["predicted"] = prediction
     predictions["errors"] = prediction - y_test
-
+    score = mean_absolute_error(y_test, prediction)
+    print(f"The mean absolute error = {score}")
     predictions_df = pd.DataFrame.from_dict(predictions)
-
     plot(predictions_df, "real", "predicted")
 
--- tools/tool_box.py
+++ tools/tool_box.py
@@ -1,14 +1,10 @@
 from re import L
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.model_selection import KFold
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold, GridSearchCV
 from sklearn.metrics import get_scorer
 from sklearn.svm import SVR
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import numpy as np
 from datetime import datetime
@@ -17,137 +13,87 @@ import sys
 import seaborn as sns
 from numpy import radians, sin, arcsin, cos, sqrt
 
-# sys.path.append(".")
-# from extraction.extract import readLRDATA
 
-
-# Define type of sklearn models for type hints
-# sklearnModel = np.union[
-#     SVR, RandomForestRegressor, KNeighborsRegressor, LinearRegression
-# ]
 def filtering_data_onehot(
-    filename: str,
-    start: datetime = datetime(2015, 12, 31),
+    filename: str = "LRData/LRDATA.csv",
+    start: datetime = datetime(2018, 1, 1),
     end: datetime = datetime(2019, 12, 31),
-    airport: str = None,
+    airport: str = "EGLL",
     airport_capacity: int = 88,
 ):
+    """Takes all the data points in a filename for a given interval of time, encodes it using it get_dummies, and focuses prediction efforts on a single airport of choosing. 
+
+    Args:
+        filename (str, optional): Filename of the .csv file to extract data from. Defaults to "LRData/LRDATA.csv".
+        start (datetime, optional): Starting point of the time interval. Defaults to datetime(2018, 1, 1).
+        end (datetime, optional): Ending point of the time interval. Defaults to datetime(2019, 12, 31).
+        airport (str, optional): Airport codename. Defaults to "EGLL" (Heathrow Airport).
+        airport_capacity (int, optional): Capacity of airport per hour defined as maximum movements per hour possible. Defaults to 88.
+
+
+    Returns:
+        tuple: Array with all relevant features of the dataset and another array of target variables.
+    """
     df = pd.read_csv(filename, header=0, index_col=0)
 
     dform = "%Y-%m-%d %H:%M:%S"
     df = df.assign(FiledOBT=lambda x: pd.to_datetime(x.FiledOBT, format=dform)).assign(
         FiledAT=lambda x: pd.to_datetime(x.FiledAT, format=dform)
     )
-    df_2 = data_filter_outliers(df, start, end)
-    df_capacity = capacity_calc(df_2, airport, airport_capacity)
+    df_2 = data_filter_outliers(df)
     if airport != None:
-        print('selecting airport....')
+        print(f"-------Selecting airport {airport}-------")
+        df_capacity = capacity_calc(df_2, airport, airport_capacity)
         df_capacity = df_capacity.query("ADES == @airport")
+    else:
+        df_capacity = df_2
     df_time_distance = time_distance(df_capacity)
     df_3 = dummies_encode(df_time_distance, airport)
     X_final = scaler(df_3)
     y = df_capacity["ArrivalDelay"].to_numpy()
 
-    pd.DataFrame((df_3)).to_csv("tools/finaldf.csv", header=False, index=False)
-    pd.DataFrame((X_final)).to_csv("tools/xdata.csv", header=False, index=False)
-    pd.DataFrame((y)).to_csv("tools/ydata.csv", header=False, index=False)
-
+    pd.DataFrame((df_3)).to_csv("data/finaldf.csv", header=False, index=False)
+    pd.DataFrame((X_final)).to_csv("data/xdata.csv", header=False, index=False)
+    print("-------Regression model DataFrame to .csv: DONE-------")
+    pd.DataFrame((y)).to_csv("data/ydata.csv", header=False, index=False)
+    print("-------Regression model target variables to .csv: DONE-------")
 
     return X_final, y
 
 
-def dummies_encode(P: pd.DataFrame, airport):
-    if airport == None:
-        new_df = P.drop(
-            ["FiledOBT", "FiledAT", "ACType", "ArrivalDelay"], axis=1
-        )
-        new_df3 = pd.get_dummies(
-            new_df, columns=["ADEP", "ADES", "ACOperator", "month", "weekday"]
-        )
-
-    else:
-        new_df = P.drop(
-            ["ADES", "FiledOBT", "FiledAT", "ACType", "ArrivalDelay"],
-            axis=1,
-        )
-        new_df3 = pd.get_dummies(
-            new_df, columns=["ADEP", "ACOperator", "month", "weekday"]
-        )
-
-    return new_df3
-
-
-def scaler(P: pd.DataFrame):
-    encoded_array = P.to_numpy()
-    scaler = MinMaxScaler()
-    X_scaled_array = scaler.fit_transform(encoded_array)
-
-    return X_scaled_array
-
-
-def filtering_data_ordinal_enc(
-    filename: str = "LRData/LRDATA.csv",
-    start: datetime = datetime(2019, 1, 1),
+def data_filter_outliers(
+    P: pd.DataFrame,
+    start: datetime = datetime(2018, 1, 1),
     end: datetime = datetime(2019, 12, 31),
 ):
-    dform = "%Y-%m-%d %H:%M:%S"
+    """Filtering outliers from the data, an outlier is determined to be either 90 min or more late, 30 min early or deperating and arriving from/on the same airport
 
-    df = pd.read_csv(filename, header=0, index_col=0)
-    df = df.assign(FiledOBT=lambda x: pd.to_datetime(x.FiledOBT, format=dform))
-    df = data_filter_outliers(df, start, end,)
+    Args:
+        P (pd.DataFrame): pandas dataframe with all flight data
+        start (datetime, optional): Starting point of the time interval. Defaults to datetime(2018, 1, 1).
+        end (datetime, optional): Ending point of the time interval. Defaults to datetime(2019, 12, 31).
 
-    new_df = df.drop(["FiledOBT", "FiledAT"], axis=1)
-    new_df2 = new_df.drop(["ArrivalDelay", "DepartureDelay"], axis=1)
+    Returns:
+        pd.DataFrame: dataframe with outliers removed
+    """
 
-    new_df3 = new_df2.to_numpy()
-    to_be_enc = new_df3[:, :6]
-
-    enc = OrdinalEncoder()
-
-    encoded_part = enc.fit_transform(to_be_enc)
-    encoded_array = np.concatenate((encoded_part, new_df3[:, 6:]), axis=1)
-    # print(f"Encoded array = {encoded_array}")
-    scaler = MinMaxScaler()
-    X_scaled_array = scaler.fit_transform(encoded_array)
-    y = df["ArrivalDelay"].to_numpy()
-
-    pd.DataFrame((X_scaled_array)).to_csv("tools/xdata.csv", header=False, index=False)
-    pd.DataFrame((y)).to_csv("tools/ydata.csv", header=False, index=False)
-
-    # print(X_scaled_array)
-    print("-------------Data filtering Done-----------------")
-    # pd.DataFrame(scaled_array).to_csv("scaled_2016_encoded_data.txt", header=False, index=False)
-
-    return X_scaled_array, y
-
-
-def get_data():
-    X = np.genfromtxt("tools/xdata.csv", delimiter=",")
-    y = np.genfromtxt("tools/ydata.csv", delimiter=",")
-
-    return X, y
-
-
-def data_filter_outliers(P: pd.DataFrame, start: datetime, end: datetime):
     P = P.query(
         "FiledOBT <= @end & FiledOBT >= @start & ArrivalDelay < 90 & ArrivalDelay > -30 & ADES != ADEP"
     )
     return P
 
 
-def data_filter_ADEPADES(P: pd.DataFrame, start: datetime, end: datetime, airport: str):
-    dform = "%Y-%m-%d %H:%M:%S"
-    P = P.assign(FiledOBT=lambda x: pd.to_datetime(x.FiledOBT, format=dform)).query(
-        "FiledOBT <= @end & FiledOBT >= @start & ArrivalDelay < 90 & ArrivalDelay > -30 & ADES != ADEP"
-    )
-    if airport != None:
-        P = P.query("ADES == @airport |ADEP == @airport ")
+def capacity_calc(P: pd.DataFrame, airport: str = "EGLL", airport_capacity: int = 88):
+    """Calculating the part capacity of airport used per 15 dependent on number of arrivals and departures and add this to the data
 
-    return P
+    Args:
+        P (pd.DataFrame): dataframe with all flight data for certain airport
+        airport (str, optional): airport for which capacity will be calculated. Defaults to "EGLL".
+        airport_capacity (int, optional): Capacity of airport per hour defined as maximum movements per hour possible. Defaults to 88.
 
-
-def capacity_calc(P: pd.DataFrame, airport: str = "EGLL", airport_capacity: int = 80):
-    # times = pd.DatetimeIndex(P.time)
+    Returns:
+        pd.DataFrame: dataframe with capacity of airport at time of flight 
+    """
     dform = "%Y-%m-%d %H:%M:%S"
     P = P.assign(FiledOBT=lambda x: pd.to_datetime(x.FiledOBT, format=dform))
 
@@ -183,6 +129,24 @@ def capacity_calc(P: pd.DataFrame, airport: str = "EGLL", airport_capacity: int 
     return new_df
 
 
+def time_distance(P: pd.DataFrame):
+    """Calculates the filed flight time of each flight and distance between 2 airports.
+
+    Args:
+        P (pd.DataFrame): DataFrame containing flights with filed arrival time and filed off block time.
+
+    Returns:
+        pd.DataFrame: Same dataframe but with a new column 'flight_time'.
+    """
+    P = P.assign(distance=lambda row: haversine(row), axis=1)
+    P["flight_time"] = P.apply(
+        lambda row: (row["FiledAT"] - row["FiledOBT"]).seconds / 60, axis=1
+    )
+    P = P.drop(["ADEPLong", "ADEPLat", "ADESLong", "ADESLat"], axis=1)
+
+    return P
+
+
 def haversine(P: pd.DataFrame):
     """Calculates the great-circle distance between two decimal
     coordinates using the Haversine formula and applies it to a dataframe.
@@ -193,21 +157,88 @@ def haversine(P: pd.DataFrame):
 
     Returns:
         pd.DataFrame: Dataframe of LRData with extra column 'distance'
-    """    
-    coords_a, coords_b = (P['ADEPLong'], P['ADEPLat']), (P['ADESLong'], P['ADESLat'])
+    """
+    coords_a, coords_b = (P["ADEPLong"], P["ADEPLat"]), (P["ADESLong"], P["ADESLat"])
     # Conversion to radians is necessary for the trigonometric functions
     phi_1, phi_2 = radians(coords_a[1]), radians(coords_b[1])
     lambda_1, lambda_2 = radians(coords_a[0]), radians(coords_b[0])
 
-    return 2 * 6371 * arcsin(sqrt(sin((phi_2 - phi_1)/2)**2 + cos(phi_1) * cos(phi_2) * sin((lambda_2 - lambda_1)/2)**2))
+    return (
+        2
+        * 6371
+        * arcsin(
+            sqrt(
+                sin((phi_2 - phi_1) / 2) ** 2
+                + cos(phi_1) * cos(phi_2) * sin((lambda_2 - lambda_1) / 2) ** 2
+            )
+        )
+    )
 
 
-def time_distance(P: pd.DataFrame):
-    P['distance'] = P.apply(lambda row: haversine(row), axis=1)
-    P['flight_time'] = P.apply(lambda row: (row['FiledAT'] - row['FiledOBT']).seconds / 60, axis=1)
-    P = P.drop(['ADEPLong', 'ADEPLat', 'ADESLong', 'ADESLat'], axis=1)
+def dummies_encode(P: pd.DataFrame, airport: str = None):
+    """Encoding the categorial features of the data. Similar to OneHotEncoder
 
-    return P
+    Args:
+        P (pd.DataFrame): Pandas DataFrame with all  flight data
+        airport ([type]): airport for which calculations will be done
+
+    Returns:
+        pd.DataFrame: Pandas DataFrame with all categorial features encoded
+    """
+    if airport == None:
+        new_df = P.drop(["FiledOBT", "FiledAT", "ACType", "ArrivalDelay"], axis=1)
+        new_df3 = pd.get_dummies(
+            new_df, columns=["ADEP", "ADES", "ACOperator", "month", "weekday"]
+        )
+
+    else:
+        new_df = P.drop(
+            ["ADES", "FiledOBT", "FiledAT", "ACType", "ArrivalDelay"], axis=1,
+        )
+        new_df3 = pd.get_dummies(
+            new_df, columns=["ADEP", "ACOperator", "month", "weekday"]
+        )
+
+    return new_df3
+
+
+def scaler(P: pd.DataFrame):
+    """Scaling the data
+
+    Args:
+        P (pd.DataFrame): pandas dataframe to be scaled
+
+    Returns:
+        pd.DataFrame: Scaled dataframe
+    """
+    encoded_array = P.to_numpy()
+    scaler = MinMaxScaler()
+    X_scaled_array = scaler.fit_transform(encoded_array)
+
+    return X_scaled_array
+
+
+def get_data(
+    folderName: str = "data",
+    fileName_x: str = "xdata.csv",
+    fileName_y: str = "ydata.csv",
+):
+    """Extracting data from csv files
+
+    Args:
+        folderName (str, optional): Name of the folder which the data is in. Defaults to "data".
+        fileName_x (str, optional): Name of the file containing the data of the features(X). Defaults to "xdata.csv".
+        fileName_y (str, optional): name of the file containing the labels(y). Defaults to "ydata.csv".
+
+    Returns:
+        [type]: [description]
+    """
+    X = np.genfromtxt(f"{folderName}/{fileName_x}", delimiter=",")
+    print(f"Data points saved under filename {fileName_x} ---- EXTRACTED.")
+    y = np.genfromtxt(f"{folderName}/{fileName_y}", delimiter=",")
+    print(f"Target var saved under filename {fileName_x} ---- EXTRACTED.")
+
+    return X, y
 
 
 def parameter_search(
@@ -256,119 +287,48 @@ def parameter_search(
     best_parameters = grid_search.best_params_
 
     if type(model) == KNeighborsRegressor:
-        best_model = KNeighborsRegressor(n_neighbors= best_parameters['n_neighbors'], weights= best_parameters['weights']) #'n_neighbors' : range(1, 100, 10), 'weights' : ["uniform"]
+        best_model = KNeighborsRegressor(
+            n_neighbors=best_parameters["n_neighbors"],
+            weights=best_parameters["weights"],
+        )  #'n_neighbors' : range(1, 100, 10), 'weights' : ["uniform"]
 
-    X_train_2, X_test, y_train_2, y_test = train_test_split(X_train, y_train, test_size=0.5, random_state=42)
+    X_train_2, X_test, y_train_2, y_test = train_test_split(
+        X_train, y_train, test_size=0.5, random_state=42
+    )
     best_model.fit(X_train_2, y_train_2)
     prediction = grid_search.predict(X_test)
 
-    
     return best_parameters, prediction, y_test
 
 
-def split_into_folds(X: np.array, y: np.array, n_folds: int):
-    """Splits the data into n amount of folds.
-       Used in double_cross_validation.
+def plot(df: pd.DataFrame, x_name: str, y_name: str):
+    """Plots predictions of a model vs. the real target values.
 
     Args:
-        X (np.array): Training data
-        y (np.array): labels corresponding to training data
-        n_folds (int): amount of folds
-
-    Returns:
-        tuple(list, list): [description]
+        df (pd.DataFrame): DataFrame containing predictions of a model and real target values.
+        x_name (str): Name of the column to be used on x-axis
+        y_name (str): Name of the column to be used on y-axis
     """
-    interval = len(y) // n_folds
-    return (
-        [X[i * interval : (i + 1) * interval] for i in range(n_folds)],
-        [y[i * interval : (i + 1) * interval] for i in range(n_folds)],
+    sns.set(style="darkgrid")
+    g = sns.jointplot(
+        x=x_name,
+        y=y_name,
+        data=df,
+        kind="kde",
+        xlim=[-30, 90],
+        ylim=[-30, 90],
+        fill=True,
+    )
+    sns.regplot(x=x_name, y=y_name, data=df, scatter=False, ax=g.ax_joint)
+    plt.show()
+
+
+if __name__ == "__main__":
+    X, y = filtering_data_onehot(
+        filename="LRData/LRDATA.csv",
+        start=datetime(2018, 1, 1),
+        end=datetime(2019, 12, 31),
+        airport="EGLL",
+        airport_capacity=88,
     )
 
-
-def double_cross_validation(
-    model,
-    parameters: dict,
-    X_train: np.array,
-    y_train: np.array,
-    score_string: str = "neg_mean_squared_error",
-    inner_folds: int = 3,
-    outer_folds: int = 10,
-    print_performance: bool = True,
-):
-    """Tunes and evaluates the performance of a model using double (nested) cross-validation.
-
-    Args:
-        model (sklearnModel): sklearn model
-        parameters (dict): dictionary with model name and dict as key with in that all hyper-parameters
-        X_train (np.array): Training + test data
-        y_train (np.array): labels corresponding to training data
-        score_func (function): which score function should be used to calculate the score of the model
-        inner_folds (int, optional): amount of inner folds used for nested cross validation. Defaults to 3.
-        outer_folds (int, optional): amount of outer folds used for nested cross validation. Defaults to 10.
-        print_performance (bool, optional): Choose if python should print best performing perameters for each model. Defaults to True.
-
-    Returns:
-        performance_score, st_dev, best_parameters [type]: [description]
-    """
-
-    scores = []
-    best_parameters = {}
-
-    X_folds, y_folds = split_into_folds(X_train, y_train, n_folds=outer_folds)
-    for i, (X_fold_test, y_fold_test) in enumerate(zip(X_folds, y_folds)):
-        # Split the data up into test fold and training data
-        X_fold_train = np.concatenate(
-            [X_folds[k] for k in range(outer_folds) if k != i]
-        )
-        y_fold_train = np.concatenate(
-            [y_folds[k] for k in range(outer_folds) if k != i]
-        )
-
-        # Perform inner cross validation to tune the model
-        grid_search = GridSearchCV(
-            model,
-            parameters,
-            cv=KFold(n_splits=inner_folds),
-            shuffle=True,
-            scoring=score_string,
-            n_jobs=-1,
-        )
-        grid_search.fit(X_fold_train, y_fold_train)
-        best_model, hyperparameters = (
-            grid_search.best_estimator_,
-            grid_search.best_params_,
-        )
-
-        # Evaluate model
-        predictions = best_model.predict(X_fold_test)
-        score_func = get_scorer(score_string)._score_func
-        score = score_func(y_fold_test, predictions)
-        scores.append(score)
-
-        # If best_model performs better than any previously found model, we update best_parameters.
-        # The if/else statements are necessary, because we need to be able to know whether a high outcome or a low
-        # outcome of score_func is desirable. For accuracy_score, a high value is desired, while for log_loss a low
-        # value is more ideal.
-        if score_func([1, 2], [1, 2]) > score_func([1, 2], [2, 1]):
-            if score == max(scores):
-                best_parameters = hyperparameters
-        else:
-            if score == min(scores):
-                best_parameters = hyperparameters
-
-    performance_score = np.mean(scores)
-    st_dev = np.std(scores)
-
-    if print_performance:
-        print(
-            f'{score_func.__name__.replace("_", " ")}: {round(performance_score, 3)}, st_dev: {round(st_dev, 5)}'
-        )
-        print(f"Tuned parameters: {best_parameters}")
-
-    return best_parameters, performance_score, st_dev
-
-def plot(df: pd.DataFrame, x_name : str, y_name :str):
-    sns.set(style='darkgrid')
-    g = sns.jointplot(x = x_name, y = y_name, data = df, kind='kde', xlim= [-30, 90], ylim= [-30, 90], fill=True)
-    sns.regplot(x = x_name, y = y_name, data = df, scatter=False, ax=g.ax_joint)
-    plt.show()
