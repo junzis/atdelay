adfd079ff0a58755e565bee43f5f4da0a52dc22f
theofalat <80906337+theofalat@users.noreply.github.com>
2021-12-15 09:50:19 +0100

    Just some small docstring changes

--- tools/tool_box.py
+++ tools/tool_box.py
@@ -66,7 +66,7 @@ def data_filter_outliers(
     start: datetime = datetime(2018, 1, 1),
     end: datetime = datetime(2019, 12, 31),
 ):
-    """Filtering outliers from the data, an outlier is determined to be either 90 min or more late, 30 min early or deperating and arriving from/on the same airport
+    """Filtering outliers from the data, an outlier is determined to be either 90 min or more late, 30 min early or departing and arriving from/on the same airport
 
     Args:
         P (pd.DataFrame): pandas dataframe with all flight data
@@ -84,12 +84,12 @@ def data_filter_outliers(
 
 
 def capacity_calc(P: pd.DataFrame, airport: str = "EGLL", airport_capacity: int = 88):
-    """Calculating the part capacity of airport used per 15 dependent on number of arrivals and departures and add this to the data
+    """Calculating the part capacity of airport used per 15, dependent on number of arrivals and departures and adding this to the data
 
     Args:
         P (pd.DataFrame): dataframe with all flight data for certain airport
         airport (str, optional): airport for which capacity will be calculated. Defaults to "EGLL".
-        airport_capacity (int, optional): Capacity of airport per hour defined as maximum movements per hour possible. Defaults to 88.
+        airport_capacity (int, optional): Capacity of airport per hour defined as maximum take-offs per hour possible. Defaults to 88.
 
     Returns:
         pd.DataFrame: dataframe with capacity of airport at time of flight 
@@ -236,7 +236,7 @@ def get_data(
     X = np.genfromtxt(f"{folderName}/{fileName_x}", delimiter=",")
     print(f"Data points saved under filename {fileName_x} ---- EXTRACTED.")
     y = np.genfromtxt(f"{folderName}/{fileName_y}", delimiter=",")
-    print(f"Target var saved under filename {fileName_x} ---- EXTRACTED.")
+    print(f"Target variables saved under filename {fileName_y} ---- EXTRACTED.")
 
     return X, y
 
