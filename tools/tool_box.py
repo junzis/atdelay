cb67486528091367a0105c5b0b8ec80d11104f1f
TimGioHog <62881743+TimGioHog@users.noreply.github.com>
2021-12-15 14:44:22 +0100

    made plot thing into function

--- tools/tool_box.py
+++ tools/tool_box.py
@@ -378,73 +378,70 @@ def flights_per_airport(
         result[airport] = len(df_airport)
     return result, airport_list
 
+def plot_flightcount_vs_error():
+    flights_per_airport_dict, airport_list = flights_per_airport()
+    acc_per_airport = {
+    "LFPO": 4.052652823141702,
+    "LOWW": 4.443855332164622,
+    "LSZH": 4.711077381748813,
+    "LPPT": 5.54990567442422,
+    "EKCH": 3.9218357147589145,
+    "LEPA": 4.279129192931119,
+    "EGCC": 4.3926117886460005,
+    "LIMC": 4.579961412211301,
+    "ENGM": 4.107187693201358,
+    "EGSS": 4.314323616406326,
+    "EBBR": 4.0016467162512095,
+    "LGAV": 4.472835467211704,
+    "EDDL": 4.084764518650214,
+    "EFHK": 3.95690555508362,
+    "LEMG": 4.7155325377479285,
+    "EPWA": 4.519428133965428,
+    "EGGW": 4.583746230814193,
+    "LSGG": 4.4874583044872365,
+    "LKPR": 4.170584482540404,
+    "EDDH": 3.7267474546350816,
+    "LHBP": 4.107814833219582,
+    "LTBA": 7.156262372123445,
+    "EGLL": 5.889489890910065,
+    "LFPG": 3.795442331570614,
+    "EHAM": 4.483325306858709,
+    "EDDF": 4.022649400353656,
+    "LEMD": 5.261546667909832,
+    "LEBL": 4.401206127258698,
+    "LTFM": 5.198203077609934,
+    "EDDM": 3.856881491942465,
+    "EGKK": 5.682978532681429,
+    "LIRF": 4.309078262385989,
+    "ULLI": 3.6039484816696996,
+    "UUEE": 7.8169804906927665,
+    "UUWW": 2.7243991225837423,
+    "LROP": 4.4270708591669266,
+    "UUDD": 4.244190217083899,
+    "EDDT": 3.6870329695533064,
+    "EGPH": 4.153829718562654,
+    "EGBB": 4.456718787221311,
+    "UKBB": 4.422615503022433,
+    "LEAL": 4.225588940008024,
+    "LPPR": 4.60775865949576,
+    "LFMN": 4.574162220773571,
+    "ESSA": 4.2143734708999,
+    "LIME": 4.432959538778985,
+    "LFLL": 4.040411700926016,
+    "EIDW": 5.696720079868284,
+    "EDDK": 3.8098529923643456,
+    "EDDS": 3.591418887028652,
+    }
 
-flights_per_airport_dict, airport_list = flights_per_airport()
-acc_per_airport = {
-"LFPO": 4.052652823141702,
-"LOWW": 4.443855332164622,
-"LSZH": 4.711077381748813,
-"LPPT": 5.54990567442422,
-"EKCH": 3.9218357147589145,
-"LEPA": 4.279129192931119,
-"EGCC": 4.3926117886460005,
-"LIMC": 4.579961412211301,
-"ENGM": 4.107187693201358,
-"EGSS": 4.314323616406326,
-"EBBR": 4.0016467162512095,
-"LGAV": 4.472835467211704,
-"EDDL": 4.084764518650214,
-"EFHK": 3.95690555508362,
-"LEMG": 4.7155325377479285,
-"EPWA": 4.519428133965428,
-"EGGW": 4.583746230814193,
-"LSGG": 4.4874583044872365,
-"LKPR": 4.170584482540404,
-"EDDH": 3.7267474546350816,
-"LHBP": 4.107814833219582,
-"LTBA": 7.156262372123445,
-"EGLL": 5.889489890910065,
-"LFPG": 3.795442331570614,
-"EHAM": 4.483325306858709,
-"EDDF": 4.022649400353656,
-"LEMD": 5.261546667909832,
-"LEBL": 4.401206127258698,
-"LTFM": 5.198203077609934,
-"EDDM": 3.856881491942465,
-"EGKK": 5.682978532681429,
-"LIRF": 4.309078262385989,
-"ULLI": 3.6039484816696996,
-"UUEE": 7.8169804906927665,
-"UUWW": 2.7243991225837423,
-"LROP": 4.4270708591669266,
-"UUDD": 4.244190217083899,
-"EDDT": 3.6870329695533064,
-"EGPH": 4.153829718562654,
-"EGBB": 4.456718787221311,
-"UKBB": 4.422615503022433,
-"LEAL": 4.225588940008024,
-"LPPR": 4.60775865949576,
-"LFMN": 4.574162220773571,
-"ESSA": 4.2143734708999,
-"LIME": 4.432959538778985,
-"LFLL": 4.040411700926016,
-"EIDW": 5.696720079868284,
-"EDDK": 3.8098529923643456,
-"EDDS": 3.591418887028652,
-}
-
-dict_2 = {}
-for airport in acc_per_airport:
-    airport_data = []
-    airport_data.append(acc_per_airport[airport])
-    airport_data.append(flights_per_airport_dict[airport])
-    dict_2[airport] = airport_data
-print('dict_2 = ', dict_2)
-airport_array = np.array([None, None])
-for airport in dict_2:
-    airport_array = np.vstack([airport_array, dict_2[airport]])
-airport_array = airport_array[1:, :].astype('float32')
-print('airport_array = ', airport_array)
-df_plot = pd.DataFrame(data = airport_array, columns=['accuracy', 'flight count'])
-print('df_plot = ', df_plot)
-plot(df_plot, 'flight count', 'accuracy')
+    dict_2 = {}
+    for airport in acc_per_airport:
+        airport_data = []
+        airport_data.append(acc_per_airport[airport])
+        airport_data.append(flights_per_airport_dict[airport])
+        dict_2[airport] = airport_data
+    airport_array = np.array([None, None])
+    for airport in dict_2:
+        airport_array = np.vstack([airport_array, dict_2[airport]])
+    airport_array = airport_array[1:, :].astype('float32')
+    df_plot = pd.DataFrame(data = airport_array, columns=['error', 'flight count'])
+    plot(df_plot, 'flight count', 'error')
