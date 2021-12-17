17d51dfbea7662bbd56a84640952c561715adfda
TimGioHog <62881743+TimGioHog@users.noreply.github.com>
2021-12-17 10:17:50 +0100

    first weather things

---
+++ extraction/weather.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import win32api
+import numpy as np
+import pandas as pd
+import pygrib
+import requests
+
+if __name__ == "__main__":
+    # print('run from weatherRun you dummy')
+    pass
---
+++ weatherRun.py
@@ -0,0 +1,2 @@
+from extraction.weather import *
+stuff()
