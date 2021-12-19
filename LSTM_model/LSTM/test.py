d8e5a540529d58373e357c0fd42525e61eadc020
Constantinos Aristodemou <50957278+ConstantinosAr@users.noreply.github.com>
2021-12-19 12:14:35 +0100

    LSTM model setup

--- LSTM_model/LSTM/test.py
+++ LSTM_model/LSTM/test.py
@@ -1,3 +1,4 @@
+# from extraction.extract import generateNNdataMultiple
 from keras.engine import input_layer
 from keras.layers.core import Activation
 import matplotlib.pyplot as plt
@@ -10,67 +11,124 @@ from keras.layers import LSTM
 from keras.layers import Input, Embedding
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 from keras.preprocessing.sequence import TimeseriesGenerator
 import numpy as np
 
 
-data = np.array([[i] for i in range(50)])
-targets = np.array([[i] for i in range(50)])
+
+#======================================================
+#                  DATA PREPARATION         
+#======================================================
+
+dataframe = pd.read_csv('EHAM_15m.csv')
+
+
+
+X, y = dataframe['planes'], dataframe['departuresDepartureDelay']
+X = X.to_numpy()
+y = y.to_numpy()
+y = np.ravel(y)
+
+X = X.reshape((-1, 1))
+
+split_percent = 0.5
+split = int(split_percent*len(X))
+
+X_train = X[:split]
+X_test = X[split:]
+
+y_train = y[:split]
+y_test = y[split:]
+
+
 look_back = 3  # how many samples back in time you want to use for your prediction
-data_gen = TimeseriesGenerator(data, targets,
+train_dataset = TimeseriesGenerator(X_train, X_train,
                                length=look_back, sampling_rate=1, #sampling_rate: the step with which the inputs are sampled for predictions
-                               batch_size=1, stride=1)  #stride: the step for the future samples we predict
+                               batch_size=10, stride=1)  #stride: the step for the future samples we predict
                                                         #batch_size: how many batches we want to use for inputs, as well as the number of
                                                         #predicted future samples
 
-# for i in range(len(data_gen)):
-#     x, y = data_gen[i]
-#     print('%s => %s' % (x, y))
-#print((data_gen[0]))
+test_dataset = TimeseriesGenerator(X_test, X_test,
+                               length=look_back, sampling_rate=1, #sampling_rate: the step with which the inputs are sampled for predictions
+                               batch_size=10, stride=1)
 
-batch_0 = data_gen[0]
 
-x, y = batch_0
 
-# x_input = np.array([1,2,3])
-# # print(x_input.reshape((2,6)))
-# print(x_input.shape)
-# # n_input, n_features = 3, 1
+#======================================================
+#               DEFINE AND FIT MODEL          
+#======================================================
+
 
 # model = Sequential()
-# # model.add(Embedding(input_dim=3, output_dim=64))
-# # model.add(Input(shape=(n_input, n_features)))
-# model.add(LSTM(100, activation='relu', input_shape=(4, 3)))
+
+# model.add(LSTM(10, activation="relu", input_shape=(look_back, 1)))
 # model.add(Dense(1))
 # model.compile(optimizer='adam', loss='mse')
 
-# print(model.summary())
 
-# #fit model
-# model.fit(x, y, steps_per_epoch=1, epochs=100, verbose=0)
-# yhat = model.predict(x_input, verbose=0)
-# print(yhat)
+# model.fit(
+#     train_dataset,
+#     epochs=5,
+#     verbose=1
+#     )
+
+# model.save('LSTM.h5')
+
+model = keras.models.load_model('LSTM.h5')
+
+#======================================================
+#        EVALUATE MODEL AND VISUALIZE PREDICTIONS
+#======================================================
+
+predictions = model.predict(test_dataset)
+
+print(predictions.shape)
+print(test_dataset[0])
+
+plt.plot(test_dataset[0], predictions, 'r')
+plt.plot(X_test, y_test, 'g')
+plt.show()
 
 
-x = np.arange(-2, 2, 0.01)
-y = 3*x
-# y = x**5 - x**4 + x**3 - x**2 - 2*x
-
-train_size_ratio = 0.7
-
-x_train = x[:int(len(x)*train_size_ratio)]
-y_train = y[:int(len(y)*train_size_ratio)]
-x_test = y[int(len(y)*train_size_ratio):]
-y_test = x[int(len(x)*train_size_ratio):]
 
 
-dataset_train = keras.preprocessing.timeseries_dataset_from_array(
-    x_train,
-    y_train,
-    sequence_length=10,
-    sampling_rate=1,
-    batch_size=3,
-)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# model.add(Dense(3, activation='relu', input_dim=1))
+
+# x_train = x[:int(len(x)*train_size_ratio)]
+# y_train = y[:int(len(y)*train_size_ratio)]
+# x_test = y[int(len(y)*train_size_ratio):]
+# y_test = x[int(len(x)*train_size_ratio):]
+
+
+# dataset_train = keras.preprocessing.timeseries_dataset_from_array(
+#     x_train,
+#     y_train,
+#     sequence_length=10,
+#     sampling_rate=1,
+#     batch_size=3,
+# )
 
 # dataset_val = keras.preprocessing.timeseries_dataset_from_array(
 #     x_test,
@@ -90,10 +148,6 @@ dataset_train = keras.preprocessing.timeseries_dataset_from_array(
 # print('=====================================')
 
 
-model = Sequential()
-model.add(Dense(3, activation='relu', input_dim=1))
-model.add(LSTM(32, return_sequences=True))
-model.add(Dense(1))
 
 
 # inputs = keras.layers.Input(shape=(inputs.shape[0], inputs.shape[1]))
@@ -101,8 +155,8 @@ model.add(Dense(1))
 # outputs = keras.layers.Dense(1)(lstm_out)
 
 # model = keras.Model(inputs=inputs, outputs=outputs)
-model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss="mse")
-model.summary()
+# model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss="mse")
+# model.summary()
 
 # path_checkpoint = "model_checkpoint.h5"
 # es_callback = keras.callbacks.EarlyStopping(monitor="val_loss", min_delta=0, patience=5)
@@ -115,19 +169,19 @@ model.summary()
 #     save_best_only=True,
 # )
 
-model.fit(
-    x_train,
-    y_train,
-    batch_size=10,
-    epochs=100,
-    validation_split=0.2
-)
+# model.fit(
+#     x_train,
+#     y_train,
+#     batch_size=10,
+#     epochs=100,
+#     validation_split=0.2
+# )
 
-predictions = model.predict(x_test)
+# predictions = model.predict(x_test)
 
-plt.plot(x_test, y_test, 'g')
-plt.plot(x_test, predictions, 'r')
-plt.show()
+# plt.plot(x_test, y_test, 'g')
+# plt.plot(x_test, predictions, 'r')
+# plt.show()
 
 # loss = history.history["loss"]
 # val_loss = history.history["val_loss"]
